2365e25d64c8d1dc327c23bd70831b8de51f34b9
miki <84585119+miko1034@users.noreply.github.com>
2025-01-28 09:45:39 +0000

    __init__ finished

--- telegram_computer_log/__init__.py
+++ telegram_computer_log/__init__.py
@@ -1,12 +1,15 @@
 import yaml
 import os
+from telegram_computer_log.main import main
 
 def start():
-	#check if config exists
+	#check if config exists, and asks for bot key.
 	if "config.yml" not in os.listdir():
-		f = open("config.yml", "w+"):
+		f = open("config.yml", "w+")
 		f.write(f"""BOT_TOKEN: {input("This is your first setup. Please enter the bot token of the bot: ")}""")
 		f.close()
+	else:
+		print("config.yml detected. Skipping first setup...")
 	
-	
-	
+	# running the main function
+	main()
